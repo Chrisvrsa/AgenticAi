c82f348e09de7d83920c86a194251c70da075e8d
Chrisvrsa <psychosisjr@gmail.com>
2025-06-11 08:44:46 -0400

    Final working code of the model

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-client.models.generate_content()
-
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(response.text)
