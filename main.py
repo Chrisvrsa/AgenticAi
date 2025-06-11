54c7bd2974c86101a14eb250e7c231c9a1b12464
Chrisvrsa <psychosisjr@gmail.com>
2025-06-11 09:25:32 -0400

    Added more code to make it user interactive in the terminal. Finished solving errors

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-
+# Self-note, sys is system and it can load terminal arguments. [0] is the name of the file, each argument after that follows like a list
 load_dotenv()
+user_prompt = sys.argv[1]
+
+# Load the api key from the environment variable .env
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+# Loading the string provided at the terminal when running python3
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
 print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
