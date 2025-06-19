2ee1de8083cecdc916bf60b3f772f8f7db246ff1
Chrisvrsa <psychosisjr@gmail.com>
2025-06-18 20:38:14 -0400

    Added starting logic for the AI to access and use functions that I have created. Error are appearing, mostly due to function_calls perhaps being only a list.

--- main.py
+++ main.py
@@ -25,15 +25,58 @@ client = genai.Client(api_key=api_key)
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
+# First function schema. This tells the AI what to do. Not define the function.
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    # list
+    function_declarations=[
+        schema_get_files_info
+    ]
+)
+# Change system prompt here
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
     contents=messages, 
-    config=types.GenerateContentConfig(system_instruction=system_prompt))
+    config=types.GenerateContentConfig(
+        tools=[available_functions], 
+        system_instruction=system_prompt))
+#//
+
+function_call_part = response.function_calls()
+# If not empty?
+if function_call_part:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
 
 # Verbose or not
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
+
 
 if verbose == "--verbose":
     print(response.text)
@@ -42,6 +85,7 @@ if verbose == "--verbose":
     print(f"Response tokens: {response_tokens}")
 else:
     print(response.text)
+    
 
 
 
